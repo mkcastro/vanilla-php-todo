095ab87643669a1d0ba18ac4544abf31b5c3f319
Mike Castro <mkcastro24@gmail.com>
2021-09-21 15:11:41 +0800

    typo

--- php/src/admin/index.php
+++ php/src/admin/index.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 
-require_once realpath('./vendor/autoload.php');
+require_once realpath('../vendor/autoload.php');
 
 use MyApp\Models\User;
 use MyApp\Models\PDODatabase;
